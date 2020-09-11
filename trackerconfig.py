1ab348a225f376f8fc30d1a9d2d3370f5b0ad8a0
David Pello <zerbitzuteknikoa.medialab@tabakalera.eu>
2020-09-11 17:39:06 +0100

    new  reset_rotor configuration value

--- trackerconfig.py
+++ trackerconfig.py
@@ -4,9 +4,10 @@ config = {
         "rotctld_addr" : 'localhost',
         "tracker_port" : 7777,
         "gqrx_port" : 7356,
-        "gqrx_addr1" : "172.16.30.82",
+        "gqrx_addr1" : "pirx2.local",
         "default_freq" : 255500000,
         "renderer_addr": "172.16.30.47",
-        "renderer_port": 7778
+        "renderer_port": 7778,
+        "reset_rotor": False
 }
 
